14d030bb0b9c3108586e5c475aba3051a43a865a
tcfplayz <58693033+ckclol@users.noreply.github.com>
2021-02-11 00:37:12 +0800

    levels 4 mobs

---
+++ src/main/java/city/spmc/levels/Level.java
@@ -0,0 +1,22 @@
+package city.spmc.levels;
+
+public enum class Level {
+    NORMAL,
+    MUTATED,
+    HARDCORE,
+    EXTREME,
+    EPIC,
+    LEGENDARY,
+    GODLY,
+    HEAVENLY,
+    CKC;
+    //normal for less then 200 hearts, white
+    //mutated for less then 1k hearts, red
+    //hardcore for less then 10k hearts, dark red
+    //extreme for less then 100k hearts, aqua
+    //epic for less then 1m hearts, dark purple
+    //legendary for less then 100m hearts, gold
+    //godly for less then 1b hearts, gray
+    //heavenly for less then 64b hearts, light purple
+    //ckc for less then 1t hesrts, lime
+}
