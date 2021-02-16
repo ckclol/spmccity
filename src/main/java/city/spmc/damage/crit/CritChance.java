fb8cdf307527765836a3b2443ce77e150014fcfa
nottcfplayz <79129019+notckclol@users.noreply.github.com>
2021-02-16 13:21:44 +0800

    Update CritChance.java

--- src/main/java/city/spmc/damage/crit/CritChance.java
+++ src/main/java/city/spmc/damage/crit/CritChance.java
@@ -1,8 +1,12 @@
 package city.spmc.damage.crit;
 
 public class CritChance {
-  // take crit chance
-  int i = math.random(1, 100)
+  ItemStack item = p.getInventory().getItemInMainHand();
+  PersistentDataContainer itemdata = item.getPersistentDataContainer();
+  PersistentDataContainer data = p.getPersistentDataContainer(); 
+  NamespacedKey e = new NamespacedKey((Plugin) this, "critchance");
+  int basecrit = data.get(e, PersistentDataType.INTEGER);
+  int i = math.random(1, 100);
   if (i>0 && i<25) {
      // insert crit chance
   }
