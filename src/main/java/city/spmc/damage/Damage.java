111e4a422b94737ea2aacd6fa031cb55767e5160
tcfplayz <chau123ka89chun10p@gmail.com>
2021-02-16 23:03:52 +0800

    Update Damage.java

--- src/main/java/city/spmc/damage/Damage.java
+++ src/main/java/city/spmc/damage/Damage.java
@@ -6,6 +6,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
@@ -15,22 +17,25 @@ public class Damage implements Listener {
     public void de(EntityDamageEvent ev) {
         Player p = (Player) ev.getEntity();
         ItemStack item = p.getInventory().getItemInMainHand();
-        PersistentDataContainer itemdata = item.getPersistentDataContainer();
+        ItemMeta meta =  item.getItemMeta();
+        PersistentDataContainer itemdata = meta.getPersistentDataContainer();
         PersistentDataContainer data = p.getPersistentDataContainer();
         NamespacedKey damage = new NamespacedKey((Plugin) this, "damage");
         NamespacedKey strength = new NamespacedKey((Plugin) this, "strength");
-        if (data.get(strength, PresistentDataType.INTEGER) + itemdata.get(strength, PersistentDataType.INTEGER) == null) {
-             int strength2 = 0;
+        int st = 0;
+        int dam = 0;
+        if (data.get(strength, PersistentDataType.INTEGER) + itemdata.get(strength, PersistentDataType.INTEGER) == 0) {
+             st = 0;
         }
         else {
-             int dam = data.get(strength, PresistentDataType.INTEGER) + itemdata.get(strength, damage, PersistentDataType.INTEGER);
+             st = data.get(strength, PersistentDataType.INTEGER) + itemdata.get(strength, PersistentDataType.INTEGER);
         }
-        if (data.get(damage, PresistentDataType.INTEGER) + itemdata.get(damage, PersistentDataType.INTEGER) == null) {
-             int dam = (int) ev.getDamage();
+        if (data.get(damage, PersistentDataType.INTEGER) + itemdata.get(damage, PersistentDataType.INTEGER) == 0) {
+             dam = (int) ev.getDamage();
         }
         else {
-             int dam = data.get(damage, PresistentDataType.INTEGER) + itemdata.get(damage, PersistentDataType.INTEGER);
+             dam = data.get(damage, PersistentDataType.INTEGER) + itemdata.get(damage, PersistentDataType.INTEGER);
         }
-        ev.setDamage(dam + strength2);
+        ev.setDamage(dam + st);
     }
 }
