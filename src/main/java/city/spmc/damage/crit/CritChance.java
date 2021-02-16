a2ff8fd99aab02e0f29e1813754818b15523438e
tcfplayz <58693033+ckclol@users.noreply.github.com>
2021-02-16 11:27:08 +0800

    Update and rename CritDamage.java to CritChance.java

---
+++ src/main/java/city/spmc/damage/crit/CritChance.java
@@ -0,0 +1,9 @@
+package city.spmc.damage.crit;
+
+public class CritChance {
+  // take crit chance
+  int i = math.random(1, 100)
+  if (i>0 && i<25) {
+     // insert crit chance
+  }
+}
--- src/main/java/city/spmc/damage/crit/CritDamage.java
+++
@@ -1,6 +0,0 @@
-package city.spmc.damage.crit;
-
-public class CritDamage {
-  int i = math.random(1, 100)
-  if (i == 1)
-}
