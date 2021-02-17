8a62c80e393c44f2f561d0523ed6272e663708a2
tcfplayz <58693033+ckclol@users.noreply.github.com>
2021-02-17 10:34:22 +0800

    BEAUTIFUL
    added some gradient

--- src/main/java/city/spmc/rarity/RarityMain.java
+++ src/main/java/city/spmc/rarity/RarityMain.java
@@ -1,6 +1,7 @@
 package city.spmc.rarity;
 
 import org.bukkit.ChatColor;
+import city.spmc.utils.Gradient;
 
 public enum RarityMain {
     BANNED(ChatColor.WHITE),
@@ -11,22 +12,29 @@ public enum RarityMain {
     EPIC(ChatColor.LIGHT_PURPLE),
     ELITE(ChatColor.GOLD),
     LEGENDARY(ChatColor.DARK_PURPLE),
-    GALAXY(ChatColor.BLACK), //rainbow
+    GALAXY(Gradient.multiRgbGradient("GALAXY", {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.AQUA, Color.BLUE, Color.PURPLE}, 7, main::linear);
     MYTHIC(ChatColor.LIGHT_PURPLE),
     GODLY(ChatColor.GOLD),
     SKYLY(ChatColor.AQUA),
-    OCEAN(ChatColor.BLUE),
-    ADMIN(ChatColor.RED),
-    HAM(ChatColor.LIGHT_PURPLE),
-    PUI(ChatColor.DARK_PURPLE);
+    OCEAN(Gradient,rgbGradient("OCEAN", ocean1, ocean2, main::linear),
+    ADMIN(ChatColor.RED);
 
     private ChatColor color;
+    private Gradient gradient;
     private RarityMain(ChatColor color) {
         this.color = color;
+    }
+    private RarityMain(Gradient gradient) {
+        this.gradient = gradient;
     }
     public ChatColor getColor() {
         return this.color;
     }
+    public Gradient getGradient() {
+        return this.gradient;
+    }
+    public Color ocean1 = new Color(9, 198, 245);
+    public Color ocean2 = new Color(4, 93, 233);
     public boolean isRarerThan(RarityMain rarity) {
         int current = this.getIndex();
         int param = rarity.getIndex();
